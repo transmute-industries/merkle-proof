81331131bb72878e1bd0229c122d16fe19a6cd43
Orie Steele <orie@transmute.industries>
2023-05-14 17:16:18 -0500

    cleaning

--- src/RFC9162_SHA256/index.ts
+++ src/RFC9162_SHA256/index.ts
@@ -1,12 +1,17 @@
 export * from './HASH'
 export * from './MTH'
+export * from './PROOF'
+export * from './PATH'
 export * from './binToHex'
 export * from './hexToBin'
 export * from './strToBin'
+
+export * from './leaf'
+export * from './treeHead'
 export * from './verifyTree'
 export * from './inclusionProof'
 export * from './verifyInclusionProof'
 export * from './consistencyProof'
 export * from './verifyConsistencyProof'
-export * from './PROOF'
-export * from './PATH'
+
+
---
+++ src/RFC9162_SHA256/treeHead.ts
@@ -0,0 +1,5 @@
+import { MTH } from "./MTH";
+
+export const treeHead = (entries: Uint8Array[]) =>{
+  return MTH(entries)
+}
